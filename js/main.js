0606858a1217b1a7d1c87fcc0a7fb9277ebceb5a
allen <liuoioi1017@gmail.com>
2021-01-12 17:16:57 +0800

    修改

--- js/main.js
+++ js/main.js
@@ -25,19 +25,6 @@ $(document).ready(function () {
 
 
 
-  // ------------------------------------------------------------------------------ //
-  // Counterup   
-  // ------------------------------------------------------------------------------ //  
-
-
-  jQuery(document).ready(function ($) {
-    $('.counter').counterUp({
-      delay: 10,
-      time: 2000
-    });
-  });
-
-
 
 
 
@@ -69,11 +56,9 @@ $(document).ready(function () {
 
 });
 
-
 $(document).ready(function () {
   $('[data-toggle="tooltip"]').tooltip();
 });
-
 
 $(function () {
   /* 按下GoTop按鈕時的事件 */
